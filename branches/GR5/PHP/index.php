0a7eb7daf3c0a70df51b52e7e6e2cc20aee4ad4e
mahmoud.arram <mahmoud.arram@068b6ae8-bc20-0410-8a79-898bd4887ca8>
2006-12-01 22:25:44 +0000

    php code almost working

--- branches/GR5/PHP/index.php
+++ branches/GR5/PHP/index.php
@@ -1,11 +1,74 @@
 <?php
+/*
+ * This code needs a major overhaul. It works but is quick and dirty.
+ * 
+ * 
+ */
+ 
+ $atmStatesFile = "C:\\OL3.3.3\\Server\\lps-3.3.3\\Kagome\\ATM\\atmstates.lzx";
+ $atmFile = "C:\\OL3.3.3\\Server\\lps-3.3.3\\Kagome\\ATM\\atm.lzx";
+ 
+	require("class.atmstate.php");
+	set_magic_quotes_runtime(0);
 	$graph = $_GET['graph'];
-	$file = "testFile.txt";
-	$fh = fopen($file, 'w');
-	fwrite($fh, $graph);
-	//fwrite($fh, print_r(json_decode($graph)));
+	$graph = stripslashes($graph);
+
+	
+	
+	
+	$graphObj = json_decode($graph, true);
+	
+	$nodes = array();
+	foreach ($graphObj["nodes"] as $node) {
+		$nObj = new atmstate($node);
+		$nObj->doXML();
+		array_push($nodes, $nObj);
+	}
+	
+	
+	$atmStates = new DOMDocument();
+	$lib = $atmStates->createElement("library");
+	$lib = $atmStates->appendChild($lib);
+	foreach ($graphObj["edges"] as $edge ) {	
+
+				$sNode = getNodeByUID($nodes, $edge["from"]);
+				$tNode = getNodeByUID($nodes, $edge["to"]);
+				$sNode->setHandler($edge["sourceButton"], $tNode->getName());
+				
+			}
+
+	foreach ($nodes as $node) {
+		$content = $atmStates->importNode($node->root, true);
+		$lib->appendChild($content);
+	}
+
+	$fh = fopen($atmStatesFile, 'w');
+	fwrite($fh, $atmStates->saveXML());
 	fclose($fh);
-	//print_r(json_decode($graph));
 	print("<OK />");
 	
+	writeATMFile($atmFile, $nodes);
+	
+	function writeATMFile($file, $nodes) {
+		$doc = new DOMDocument();
+		$doc->load($file);	
+		$atmNode = $doc->getElementsByTagName("atm")->item(0);
+		$i = 0;
+		foreach ($nodes as $node) {
+			$s = $doc->createElement($node->getName());
+			$s->setAttribute("name", $node->getName());
+			$i == 0 ? $s->setAttribute("apply", "true") : $s->setAttribute("apply", "false");
+			$atmNode->appendChild($s);
+			$i++;
+		}
+		//$atmNode = $newAtmNode;
+		$doc->save($file);
+	}
+	
+	function getNodeByUID($nodes, $uid) {
+		foreach ($nodes as $node) {
+			if ($node->uid == $uid) return $node;
+			
+		}
+	}
 ?>
